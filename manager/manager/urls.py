cb26a96fe3217514d111bae9a679c0d60dd41c17
MelodyDuplaix <melo.surseine@gmail.com>
2024-12-22 23:22:54 +0100

    Update du readme

--- manager/manager/urls.py
+++ manager/manager/urls.py
@@ -14,14 +14,18 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin # type: ignore
-from django.urls import path, include # type: ignore
+from django.contrib import admin  # type: ignore
 from django.contrib.auth import views as auth_views
-from tasks.views import ResetPasswordView
-from tasks import views
+from django.urls import path, include  # type: ignore
+from tasks import views  # type: ignore
+from tasks.views import ResetPasswordView  # type: ignore
 
 urlpatterns = [
     path('admin/', admin.site.urls, name='admin'),
+    path('password_change/',
+         auth_views.PasswordChangeView.as_view(template_name='registration/password_change_form.html'),
+         name='password_change'),
+    path('accounts/', include('django.contrib.auth.urls')),
     path('accounts/login/', auth_views.LoginView.as_view(), name='login'),
     path('accounts/signup/', views.signup, name='signup'),
     path('accounts/profile/', views.profile, name='profile'),
--- readme.md
+++ readme.md
@@ -106,7 +106,7 @@ amélioration.
 - [x] Ajouter l'utilisation de la méthode login après l'inscription et avant la redirection pour que l'utilisateur soit
   directement connecté après inscription, et / ou ajouter un message de confirmation
 - [x] Ajout d'une app bar en haut de la page
-- [ ] Pouvoir ajouter un type de tache en ajoutant une tache
+- [x] Pouvoir ajouter un type de tache en ajoutant une tache
 - [ ] Améliorer l'authentification (oubli de mot de passe, etc)
 - [ ] Refaire les tests unitaires
 - [ ] avoir la possibilité de timers pour mesurer le temps sur les taches (bouton finir la journée, et enregistrement
