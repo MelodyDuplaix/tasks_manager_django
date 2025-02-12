68b27193cc314b19ad78bf8308f6943166ab9e46
MelodyDuplaix <melo.surseine@gmail.com>
2025-02-12 15:57:22 +0100

    Fix: Filtre les actions par numéro de pièces dans la récupération d'historique

--- manager/tasks/views.py
+++ manager/tasks/views.py
@@ -111,8 +111,9 @@ def options(request):
     submanagers = SubManager.objects.filter(user=request.user)
 
     # Filtrer les actions pour l'utilisateur connecté
-    history = Action.objects.filter(date__date=date.today(), sub_manager__user=request.user)
+    history = Action.objects.filter(coins_number__gt=0, date__date=date.today(), sub_manager__user=request.user)
     total_history = Action.objects.filter(sub_manager__user=request.user)
+    # filtrer les actions total
 
     # Calculer le total des coins et objectifs
     total_total_coins = sum(action.coins_number for action in total_history if action.sub_manager.active)
